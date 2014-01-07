aba7c86526e850f6ef44f0e6cc8daa1b89a8c65b
Nicolas Froidure <nicolas_froidure@elitwork.com>
2014-01-07 19:17:29 +0100

    Fix for appendCodepoints

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ var svgicons2svgfont = require('svgicons2svgfont')
   , Path = require('path')
   , gutil = require('gulp-util')
   , Stream = require('stream')
+  , Fs = require('fs')
 ;
 
 module.exports = function(options) {
@@ -42,55 +43,68 @@ module.exports = function(options) {
         [].slice.call(arguments, 0).concat()));
     };
 
+    // Map each icons to their correspondinf glyphs
+    var glyphs = files.map(function(file) {
+      // Creating an object for each icon
+      var matches = Path.basename(file.path).match(/^(?:u([0-9a-f]{4})\-)?(.*).svg$/i)
+        , glyph = {
+          name: matches[2],
+          codepoint: 0,
+          file: file.path,
+          stream: file.pipe(new Stream.PassThrough())
+        };
+      if(matches&&matches[1]) {
+        glyph.codepoint = parseInt(matches[1], 16);
+        usedCodePoints.push(glyph.codepoint);
+        return glyph;
+      }
+      return glyph;
+    }).map(function(glyph){
+      if(0 === glyph.codepoint) {
+        do {
+          glyph.codepoint = curCodepoint++;
+        } while(-1 !== usedCodePoints.indexOf(glyph.codepoint))
+        usedCodePoints.push(glyph.codepoint);
+        if(options.appendCodepoints) {
+          glyph.stream.on('end', function() {
+            Fs.rename(glyph.file, Path.dirname(glyph.file) + '/'
+              + 'u' + glyph.codepoint.toString(16).toUpperCase()
+              + '-' + glyph.name + '.svg',
+              function(err) {
+                if(err) {
+                  gutil.log("Could not save codepoint: "
+                    + 'u' + i.toString(16).toUpperCase()
+                    +' for ' + glyph.name + '.svg');
+                } else {
+                  gutil.log("Saved codepoint: "
+                    + 'u' + glyph.codepoint.toString(16).toUpperCase()
+                    +' for ' + glyph.name + '.svg');
+                }
+              }
+            );
+          });
+        }
+      }
+      return glyph;
+    });
+
+    glyphs.forEach(function(glyph){
+      glyph.stream.on('end', function() {
+        
+      });
+    });
+
     // Create the font file
     var joinedFile = new gutil.File({
       cwd: files[0].cwd,
       base: files[0].base,
-      path: Path.join(files[0].base, options.fontName) + '.svg',
-      contents: svgicons2svgfont(files.map(function(file) {
-        // Creating an object for each icon
-        var matches = Path.basename(file.path).match(/^(?:u([0-9a-f]{4})\-)?(.*).svg$/i)
-          , glyph = {
-            name: matches[2],
-            codepoint: 0,
-            file: file.path,
-            stream: file.pipe(new Stream.PassThrough())
-          };
-        if(matches&&matches[1]) {
-          glyph.codepoint = parseInt(matches[1], 16);
-          usedCodePoints.push(glyph.codepoint);
-          return glyph;
-        }
-        return glyph;
-      }).map(function(glyph){
-        if(0 === glyph.codepoint) {
-          do {
-            glyph.codepoint = curCodepoint++;
-          } while(-1 !== usedCodePoints.indexOf(glyph.codepoint))
-          usedCodePoints.push(glyph.codepoint);
-          if(options.appendCodepoints) {
-            glyph.stream.on('end', function() {
-              Fs.rename(glyph.file, Path.dirname(glyph.file) + '/'
-                + 'u' + glyph.codepoint.toString(16).toUpperCase()
-                + '-' + glyph.name + '.svg',
-                function(err) {
-                  if(err) {
-                    gutil.log("Could not save codepoint: "
-                      + 'u' + i.toString(16).toUpperCase()
-                      +' for ' + glyph.name + '.svg');
-                  } else {
-                    gutil.log("Saved codepoint: "
-                      + 'u' + glyph.codepoint.toString(16).toUpperCase()
-                      +' for ' + glyph.name + '.svg');
-                  }
-                }
-              );
-            });
-          }
-        }
-        return glyph;
-      }), options)
+      path: Path.join(files[0].base, options.fontName) + '.svg'
     });
+    
+    // Running the parent library
+    joinedFile.contents = svgicons2svgfont(glyphs, options);
+
+    // Giving the font back to the stream
     if(files[0].isBuffer()) {
       var buf = new Buffer('');
       joinedFile.contents.on('data', function(chunk) {
--- tests/tests.mocha.js
+++ tests/tests.mocha.js
@@ -9,7 +9,7 @@ var fs = require('fs')
 
 describe('gulp-svgicons2svgfont', function() {
 
-  describe('in strem mode', function() {
+  describe('in stream mode', function() {
 
     it('should work with cleanicons', function(done) {
       gulp.src(__dirname + '/fixtures/cleanicons/*.svg', {buffer: false})
@@ -62,13 +62,32 @@ describe('gulp-svgicons2svgfont', function() {
         });
     });
 
+    it('should work with unprefixed icons (stream)', function(done) {
+      gulp.src(__dirname + '/fixtures/unprefixed/*.svg', {buffer: false})
+        .pipe(gulp.dest(__dirname + '/fixtures/unprefixedcopy/'));/*
+        .pipe(svgicons2svgfont({
+          fontName: 'originalicons'
+        })).on('data', function(file) {
+          assert.equal(file.isStream(), true);
+          file.pipe(es.wait(function(err, data) {
+            assert.equal(err, undefined);
+            assert.equal(
+              data,
+              fs.readFileSync(__dirname + '/expected/test-originalicons-font.svg', 'utf8')
+            );
+            done();
+          }));
+        });*/
+        done();
+    });
+
   });
 
 
   describe('in buffer mode', function() {
 
     it('should work with cleanicons', function(done) {
-      gulp.src('tests/fixtures/cleanicons/*.svg')
+      gulp.src('tests/fixtures/cleanicons/*.svg', {buffer: true})
         .pipe(svgicons2svgfont({
           fontName: 'cleanicons'
         })).on('data', function(file) {
@@ -82,7 +101,7 @@ describe('gulp-svgicons2svgfont', function() {
     });
 
     it('should work with prefixedicons', function(done) {
-      gulp.src(__dirname + '/fixtures/prefixedicons/*.svg')
+      gulp.src(__dirname + '/fixtures/prefixedicons/*.svg', {buffer: true})
         .pipe(svgicons2svgfont({
           fontName: 'prefixedicons'
         })).on('data', function(file) {
@@ -96,7 +115,7 @@ describe('gulp-svgicons2svgfont', function() {
     });
 
     it('should work with originalicons', function(done) {
-      gulp.src(__dirname + '/fixtures/originalicons/*.svg')
+      gulp.src(__dirname + '/fixtures/originalicons/*.svg', {buffer: true})
         .pipe(svgicons2svgfont({
           fontName: 'originalicons'
         })).on('data', function(file) {
@@ -115,7 +134,7 @@ describe('gulp-svgicons2svgfont', function() {
   describe('Using gulp.dest in buffer mode', function() {
 
     it('should work with cleanicons', function(done) {
-      gulp.src(__dirname + '/fixtures/cleanicons/*.svg')
+      gulp.src(__dirname + '/fixtures/cleanicons/*.svg', {buffer: true})
         .pipe(svgicons2svgfont({
           fontName: 'cleanicons'
         }))
@@ -133,6 +152,65 @@ describe('gulp-svgicons2svgfont', function() {
             fs.rmdirSync(__dirname + '/results/');
             done();
         });
+    });
+
+    it('should work with unprefixed icons (stream)', function(done) {
+      gulp.src(__dirname + '/fixtures/unprefixedicons/*.svg', {buffer: true})
+        .pipe(gulp.dest(__dirname + '/results/unprefixedicons/'))
+        .pipe(es.wait(function() {
+          gulp.src(__dirname + '/results/unprefixedicons/*.svg', {buffer: true})
+            .pipe(svgicons2svgfont({
+              fontName: 'unprefixedicons',
+              appendCodepoints: true
+            }))
+            .pipe(es.map(function(file, cb) {
+              console.log('test', file.path);
+              cb(null, file);
+            }))
+            .pipe(gulp.dest(__dirname + '/results/'))
+            .on('data', function(file) {
+              assert.equal(fs.existsSync(__dirname
+                + '/results/unprefixedicons/uE001-arrow-down.svg'), true);
+              assert.equal(
+                fs.readFileSync(__dirname + '/results/unprefixedicons/uE001-arrow-down.svg', 'utf8'),
+                fs.readFileSync(__dirname + '/fixtures/unprefixedicons/arrow-down.svg', 'utf8')
+              );
+              assert.equal(fs.existsSync(__dirname
+                + '/results/unprefixedicons/uE002-arrow-left.svg'), true);
+              assert.equal(
+                fs.readFileSync(__dirname + '/results/unprefixedicons/uE002-arrow-left.svg', 'utf8'),
+                fs.readFileSync(__dirname + '/fixtures/unprefixedicons/arrow-left.svg', 'utf8')
+              );
+              assert.equal(fs.existsSync(__dirname
+                + '/results/unprefixedicons/uE003-arrow-right.svg'), true);
+              assert.equal(
+                fs.readFileSync(__dirname + '/results/unprefixedicons/uE003-arrow-right.svg', 'utf8'),
+                fs.readFileSync(__dirname + '/fixtures/unprefixedicons/arrow-right.svg', 'utf8')
+              );
+              assert.equal(fs.existsSync(__dirname
+                + '/results/unprefixedicons/uE004-arrow-up.svg'), true);
+              assert.equal(
+                fs.readFileSync(__dirname + '/results/unprefixedicons/uE004-arrow-up.svg', 'utf8'),
+                fs.readFileSync(__dirname + '/fixtures/unprefixedicons/arrow-up.svg', 'utf8')
+              );
+              assert.equal(file.isBuffer(), true);
+              file.pipe(es.wait(function(err, data) {
+                assert.equal(err, undefined);
+                assert.equal(
+                  data,
+                  fs.readFileSync(__dirname + '/expected/test-unprefixedicons-font.svg', 'utf8')
+                );
+                fs.unlinkSync(__dirname + '/results/unprefixedicons/uE001-arrow-down.svg');
+                fs.unlinkSync(__dirname + '/results/unprefixedicons/uE002-arrow-left.svg');
+                fs.unlinkSync(__dirname + '/results/unprefixedicons/uE003-arrow-right.svg');
+                fs.unlinkSync(__dirname + '/results/unprefixedicons/uE004-arrow-up.svg');
+                fs.rmdirSync(__dirname + '/results/unprefixedicons/');
+                fs.unlinkSync(__dirname + '/results/unprefixedicons.svg');
+                fs.rmdirSync(__dirname + '/results/');
+                done();
+              }));
+            });
+        }));
     });
 
   });
