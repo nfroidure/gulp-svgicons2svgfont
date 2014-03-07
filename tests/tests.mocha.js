e8c9c730911c950d2abd4913547d0114f7bc134a
Nicolas Froidure <nicolas_froidure@elitwork.com>
2014-03-07 10:21:37 +0100

    No need to write codepoints file

--- tests/tests.mocha.js
+++ tests/tests.mocha.js
@@ -189,20 +189,19 @@ describe('gulp-svgicons2svgfont', function() {
     });
 
     it('should emit an event with the codepoint mapping', function(done) {
+      var codepoints;
       gulp.src(__dirname + '/fixtures/cleanicons/*.svg', {buffer: false})
         .pipe(svgicons2svgfont({
           fontName: 'cleanicons'
-        })).on('codepoints', function(codepoints) {
-          fs.mkdirSync(__dirname+'/results/');
-          fs.writeFileSync(__dirname+'/results/test-codepoints.json', JSON.stringify(codepoints));
+        })).on('codepoints', function(cpts) {
+          codepoints = cpts;
         }).on('data', function(file) {
           assert.equal(file.isStream(), true);
           file.pipe(es.wait(function(err, data) {
             assert.equal(err, undefined);
-            assert.equal(
-              fs.readFileSync(__dirname+'/results/test-codepoints.json', 'utf8'),
-              fs.readFileSync(__dirname+'/expected/test-codepoints.json', 'utf8')
-            );
+            assert.deepEqual(
+              codepoints, JSON.parse(fs.readFileSync(
+                __dirname + '/expected/test-codepoints.json', 'utf8')) );
             done();
           }));
         });
