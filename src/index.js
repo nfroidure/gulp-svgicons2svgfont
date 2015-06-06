2991d5a6ae9e2b1e90c8d0dcad8f6666f14b6a33
Nicolas Froidure <nicolas.froidure@insertafter.com>
2015-06-06 17:40:45 +0200

    Upgrade svgicons2svgfont 2nd major version

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ var gutil = require('gulp-util');
 var Stream = require('readable-stream');
 var Fs = require('fs');
 var path = require('path');
+var plexer = require('plexer');
 
 module.exports = function(options) {
   var files = [];
   var usedCodePoints = [];
   var curCodepoint;
+  var firstFile = null;
 
   options = options || {};
   options.ignoreExt = options.ignoreExt || false;
@@ -22,120 +24,125 @@ module.exports = function(options) {
       [].slice.call(arguments, 0).concat()));
   };
 
-  var stream = new Stream.Transform({objectMode: true});
+  // Emit event containing codepoint mapping
+  options.callback = function(glyphs) {
+    stream.emit('codepoints', glyphs.map(function(glyph) {
+      return {
+        name: glyph.name,
+        codepoint: glyph.unicode.charCodeAt(0)
+      };
+    }));
+  };
+  var inputStream = new Stream.Transform({objectMode: true});
+  var outputStream = new Stream.PassThrough({objectMode: true});
+  var stream = plexer({objectMode: true}, inputStream, outputStream);
+  var fontStream;
 
   options.error = options.error || function() {
     stream.emit('error', new PluginError('svgicons2svgfont',
       [].slice.call(arguments, 0).concat()));
   };
 
-  // Collecting icons
-  stream._transform = function bufferContents(file, unused, done) {
+  inputStream._transform  = function _gulpSVGIcons2SVGFontTransform(file, unused, done) {
     // When null just pass through
     if(file.isNull()) {
-      stream.push(file); done();
+      outputStream.write(file); done();
       return;
     }
 
     // If the ext doesn't match, pass it through
     if((!options.ignoreExt) && '.svg' !== path.extname(file.path)) {
-      stream.push(file); done();
+      outputStream.write(file); done();
       return;
     }
 
-    files.push(file);
+    // Wrap icons for the underlying lib
+    var matches = path.basename(file.path)
+      .match(/^(?:u([0-9a-f]{4,6})\-)?(.*).svg$/i);
+    var metadata = {
+      name: matches[2],
+      unicode: ''
+    };
+    if(matches && matches[1]) {
+      metadata.unicode = String.fromCharCode(parseInt(matches[1], 16));
+      usedCodePoints.push(metadata.unicode);
+    } else {
+      do {
+        metadata.unicode = String.fromCharCode(curCodepoint++);
+      } while(-1 !== usedCodePoints.indexOf(metadata.unicode));
+      usedCodePoints.push(metadata.unicode);
+      if(options.appendCodepoints) {
+        Fs.rename(file.path, path.dirname(file.path) + '/' +
+          'u' + metadata.unicode.charCodeAt(0).toString(16).toUpperCase() +
+          '-' + metadata.name + '.svg',
+          function(err) {
+            if(err) {
+              gutil.log('Could not save codepoint: ' +
+                'u' + metadata.unicode.charCodeAt(0).toString(16).toUpperCase() +
+                ' for ' + metadata.name + '.svg');
+            } else {
+              gutil.log('Saved codepoint: ' +
+                'u' + metadata.unicode.charCodeAt(0).toString(16).toUpperCase() +
+                ' for ' + metadata.name + '.svg');
+            }
+          }
+        );
+      }
+    }
+
+    // Generating the font
+    if(!firstFile) {
+      fontStream = svgicons2svgfont(options);
+      firstFile = file;
+      // Create the font file
+      var joinedFile = new gutil.File({
+        cwd: firstFile.cwd,
+        base: firstFile.base,
+        path: path.join(firstFile.base, options.fontName) + '.svg'
+      });
+
+      // Giving the font back to the stream
+      if(firstFile.isBuffer()) {
+        var buf = new Buffer('');
+        fontStream.on('data', function(chunk) {
+          buf = Buffer.concat([buf, chunk], buf.length + chunk.length);
+        });
+        fontStream.on('end', function() {
+          joinedFile.contents = buf;
+          outputStream.push(joinedFile);
+          outputStream.end();
+        });
+      } else {
+        joinedFile.contents = fontStream;
+        outputStream.push(joinedFile);
+        fontStream.on('end', function() {
+          outputStream.end();
+        });
+      }
+    }
+    var iconStream;
+    if(file.isBuffer()) {
+      iconStream = new Stream.PassThrough();
+      setImmediate(function(argument) {
+        iconStream.write(file.contents);
+        iconStream.end();
+      });
+    } else {
+      iconStream = file.contents;
+    }
+    iconStream.metadata = metadata;
+    fontStream.write(iconStream);
 
     done();
   };
 
-  // Generating the font
-  stream._flush = function endStream(done) {
-
-    // No icons, exit
-    if(files.length === 0) return done();
-
-    // Map each icons to their corresponding glyphs
-    var glyphs = files.map(function(file) {
-      // Creating an object for each icon
-      var matches = path.basename(file.path)
-        .match(/^(?:u([0-9a-f]{4,6})\-)?(.*).svg$/i);
-      var glyph = {
-        name: matches[2],
-        codepoint: 0,
-        file: file.path,
-        stream: file.pipe(new Stream.PassThrough())
-      };
-      if(matches && matches[1]) {
-        glyph.codepoint = parseInt(matches[1], 16);
-        usedCodePoints.push(glyph.codepoint);
-      }
-      return glyph;
-    }).map(function(glyph) {
-      if(0 === glyph.codepoint) {
-        do {
-          glyph.codepoint = curCodepoint++;
-        } while(-1 !== usedCodePoints.indexOf(glyph.codepoint));
-        usedCodePoints.push(glyph.codepoint);
-        if(options.appendCodepoints) {
-          Fs.rename(glyph.file, path.dirname(glyph.file) + '/' +
-            'u' + glyph.codepoint.toString(16).toUpperCase() +
-            '-' + glyph.name + '.svg',
-            function(err) {
-              if(err) {
-                gutil.log('Could not save codepoint: ' +
-                  'u' + glyph.codepoint.toString(16).toUpperCase() +
-                  ' for ' + glyph.name + '.svg');
-              } else {
-                gutil.log('Saved codepoint: ' +
-                  'u' + glyph.codepoint.toString(16).toUpperCase() +
-                  ' for ' + glyph.name + '.svg');
-              }
-            }
-          );
-        }
-      }
-      return glyph;
-    });
-
-    glyphs.forEach(function(glyph) {
-      glyph.stream.on('end', function() {
-
-      });
-    });
-
-    // Create the font file
-    var joinedFile = new gutil.File({
-      cwd: files[0].cwd,
-      base: files[0].base,
-      path: path.join(files[0].base, options.fontName) + '.svg'
-    });
-
-    // Running the parent library
-    joinedFile.contents = svgicons2svgfont(glyphs, options);
-
-    // Emit event containing codepoint mapping
-    stream.emit('codepoints', glyphs.map(function(glyph) {
-      return {
-        name: glyph.name,
-        codepoint: glyph.codepoint
-      };
-    }));
-
-    // Giving the font back to the stream
-    if(files[0].isBuffer()) {
-      var buf = new Buffer('');
-      joinedFile.contents.on('data', function(chunk) {
-        buf = Buffer.concat([buf, chunk], buf.length + chunk.length);
-      });
-      joinedFile.contents.on('end', function() {
-        joinedFile.contents = buf;
-        stream.push(joinedFile);
-        done();
-      });
+  inputStream._flush  = function _gulpSVGIcons2SVGFontFlush(done) {
+    if(!firstFile) {
+      outputStream.end();
     } else {
-      stream.push(joinedFile);
-      done();
+      fontStream.end();
     }
+    done();
   };
 
   return stream;
