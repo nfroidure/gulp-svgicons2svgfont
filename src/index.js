09191b62fd3dcf36babe6f4cc23f1823a65e2939
Nicolas Froidure <nicolas.froidure@insertafter.com>
2015-07-25 22:27:23 +0200

    Suppress linter warnings

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
+'use strict';
+
 var svgicons2svgfont = require('svgicons2svgfont');
 var gutil = require('gulp-util');
 var Stream = require('readable-stream');
-var Fs = require('fs');
 var path = require('path');
 var plexer = require('plexer');
 var fileSorter = require('svgicons2svgfont/src/filesorter');
 
 module.exports = function(options) {
   var filesBuffer = [];
-  var streamsBuffer = [];
-  var firstFile = null;
   var metadataProvider;
+  var inputStream = new Stream.Transform({ objectMode: true });
+  var outputStream = new Stream.PassThrough({ objectMode: true });
+  var stream = plexer({ objectMode: true }, inputStream, outputStream);
+  var fontStream;
 
   options = options || {};
   options.ignoreExt = options.ignoreExt || false;
-  options.startUnicode = options.startUnicode ||  0xEA01;
+  options.startUnicode = options.startUnicode || 0xEA01;
   options.appendUnicode = !!options.appendUnicode;
 
   if(!options.fontName) {
@@ -31,26 +34,24 @@ module.exports = function(options) {
     stream.emit('glyphs', glyphs.map(function(glyph) {
       return {
         name: glyph.name,
-        unicode: glyph.unicode
+        unicode: glyph.unicode,
       };
     }));
   };
-  var inputStream = new Stream.Transform({objectMode: true});
-  var outputStream = new Stream.PassThrough({objectMode: true});
-  var stream = plexer({objectMode: true}, inputStream, outputStream);
-  var fontStream;
 
   options.error = options.error || function() {
-    stream.emit('error', new PluginError('svgicons2svgfont',
+    stream.emit('error', new gutil.PluginError('svgicons2svgfont',
       [].slice.call(arguments, 0).concat()));
   };
 
   metadataProvider = options.metadataProvider || require('svgicons2svgfont/src/metadata')({
     startUnicode: options.startUnicode,
-    appendUnicode: options.appendUnicode
+    appendUnicode: options.appendUnicode,
   });
 
-  inputStream._transform  = function _gulpSVGIcons2SVGFontTransform(file, unused, done) {
+  inputStream._transform = function _gulpSVGIcons2SVGFontTransform(file, unused, done) {
+    var fontFile = null;
+
     // When null just pass through
     if(file.isNull()) {
       outputStream.write(file); done();
@@ -65,32 +66,31 @@ module.exports = function(options) {
 
     if(0 === filesBuffer.length) {
       // Generating the font
-      var firstFile = file;
       fontStream = svgicons2svgfont(options);
-      fontStream.on('error', function (error) {
-        outputStream.emit('error', error);
+      fontStream.on('error', function(err) {
+        outputStream.emit('error', err);
       });
       // Create the font file
-      var joinedFile = new gutil.File({
-        cwd: firstFile.cwd,
-        base: firstFile.base,
-        path: path.join(firstFile.base, options.fontName) + '.svg'
+      fontFile = new gutil.File({
+        cwd: file.cwd,
+        base: file.base,
+        path: path.join(file.base, options.fontName) + '.svg',
       });
 
       // Giving the font back to the stream
-      if(firstFile.isBuffer()) {
+      if(file.isBuffer()) {
         var buf = new Buffer('');
         fontStream.on('data', function(chunk) {
           buf = Buffer.concat([buf, chunk], buf.length + chunk.length);
         });
         fontStream.on('end', function() {
-          joinedFile.contents = buf;
-          outputStream.push(joinedFile);
+          fontFile.contents = buf;
+          outputStream.push(fontFile);
           outputStream.end();
         });
       } else {
-        joinedFile.contents = fontStream;
-        outputStream.push(joinedFile);
+        fontFile.contents = fontStream;
+        outputStream.push(fontFile);
         outputStream.end();
       }
     }
@@ -101,7 +101,7 @@ module.exports = function(options) {
     done();
   };
 
-  inputStream._flush  = function _gulpSVGIcons2SVGFontFlush(done) {
+  inputStream._flush = function _gulpSVGIcons2SVGFontFlush(done) {
     if(!filesBuffer.length) {
       outputStream.end();
       return done();
@@ -115,9 +115,10 @@ module.exports = function(options) {
     // Wrap icons for the underlying lib
     filesBuffer.forEach(function(file) {
       var iconStream;
+
       if(file.isBuffer()) {
         iconStream = new Stream.PassThrough();
-        setImmediate(function(argument) {
+        setImmediate(function() {
           iconStream.write(file.contents);
           iconStream.end();
         });
