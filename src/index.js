4cca1feada288d1457f5fd3438ad71cdeed6ddaf
Tyler <tyler.z.yang@gmail.com>
2015-12-28 12:53:40 +0800

    Update unit tests, change undefined to null when there is not fill attribute in svg element

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ module.exports = function(options) {
       return {
         name: glyph.name,
         unicode: glyph.unicode,
-        color: glyph.color
+        color: glyph.color || null
       };
     }));
   };
