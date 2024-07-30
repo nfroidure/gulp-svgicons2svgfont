80ddf32cc4493ef5143244dffd249d56a9ff36d8
Nicolas Froidure <nicolas.froidure@gmail.com>
2024-07-30 17:41:41 +0200

    chore(dependencies): update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,101 +1,100 @@
-'use strict'
+import {
+  SVGIcons2SVGFontStream,
+  SVGIconsDirStream,
+  getMetadataService,
+  type SVGIcons2SVGFontStreamOptions,
+} from 'svgicons2svgfont';
+import glob from 'glob';
+import PluginError from 'plugin-error';
+import { Readable } from 'node:stream';
+import Vinyl from 'vinyl';
 
-// @ts-ignore
-import SVGIcon2SVGFontStream from 'svgicons2svgfont'
-// @ts-ignore
-import SVGIconsDirStream from 'svgicons2svgfont/src/iconsdir'
-// @ts-ignore
-import defaultMetadataProvider from 'svgicons2svgfont/src/metadata'
-import log from 'fancy-log'
-import glob from 'glob'
-import PluginError from 'plugin-error'
-import Stream, { Readable } from 'readable-stream'
-import Vinyl from 'vinyl'
+const PLUGIN_NAME = 'gulp-svgicons2svgfont';
 
-namespace svgicons2svgfont {
-  export interface Glyph {
-    name: string
-    unicode: {}
-    color?: string
-  }
-
-  export interface Options {
-    startUnicode?: number
-    prependUnicode?: boolean
-    fileName?: string
-    fontName: string
-    log?: (msg: string) => void
-    metadataProvider?: (filePath: String, cb: (error: any, metadata: any) => void) => void
-    error: (this: Readable, error: string) => void
-    callback: (glyphs: Glyph[]) => void
-  }
+export interface Glyph {
+  name: string;
+  unicode: string[];
+  color?: string;
 }
-const svgicons2svgfont = (globs: string | string[], options: svgicons2svgfont.Options) => {
+
+export type SVGIcons2SVGFontOptions = Partial<SVGIcons2SVGFontStreamOptions> & {
+  startUnicode?: number;
+  prependUnicode?: boolean;
+  fileName?: string;
+  fontName: string;
+  metadataProvider?: ReturnType<typeof getMetadataService>;
+  callback?: (glyphs: Glyph[]) => void;
+};
+
+export const svgicons2svgfont = (
+  globs: string | string[],
+  options: SVGIcons2SVGFontOptions,
+) => {
   // const inputStream = new Stream.Transform({ objectMode: true })
-  const resultStream = new Stream.Readable({ objectMode: true })
+  const resultStream = new Readable({ objectMode: true });
 
-  options.startUnicode = options.startUnicode || 0xea01
-  options.prependUnicode = !!options.prependUnicode
-  options.fileName = options.fileName || options.fontName
+  options.startUnicode = options.startUnicode || 0xea01;
+  options.prependUnicode = !!options.prependUnicode;
+  options.fileName = options.fileName || options.fontName;
 
-  if ((options as any).appendUnicode) {
+  if ((options as unknown as { appendUnicode: boolean }).appendUnicode) {
     throw new PluginError(
       'svgicons2svgfont',
       'The "appendUnicode" option was renamed to "prependUnicode".' +
         ' See https://github.com/nfroidure/gulp-svgicons2svgfont/issues/33',
-    )
+    );
   }
 
   if (!options.fontName) {
-    throw new PluginError('svgicons2svgfont', 'Missing options.fontName')
+    throw new PluginError('svgicons2svgfont', 'Missing options.fontName');
   }
-
-  options.log =
-    options.log ||
-    function(...args) {
-      log('gulp-svgicons2svgfont:', ...args)
-    }
 
   // Emit event containing codepoint mapping
-  options.callback = function(glyphs) {
+  options.callback = function (glyphs) {
     resultStream.emit(
       'glyphs',
-      glyphs.map(glyph => {
-        const finalGlyph: svgicons2svgfont.Glyph = {
+      glyphs.map((glyph) => {
+        const finalGlyph: Glyph = {
           name: glyph.name,
           unicode: glyph.unicode,
-        }
+        };
 
         if (glyph.color) {
-          finalGlyph.color = glyph.color
+          finalGlyph.color = glyph.color;
         }
-        return finalGlyph
+        return finalGlyph;
       }),
-    )
-  }
+    );
+  };
 
-  options.error =
-    options.error ||
-    function(error: string) {
-      this.emit('error', new PluginError('svgicons2svgfont', error))
-    }
   options.metadataProvider =
     options.metadataProvider ||
-    defaultMetadataProvider({
+    getMetadataService({
       startUnicode: options.startUnicode,
       prependUnicode: options.prependUnicode,
-    })
+    });
 
-  const globsArray: string[] = 'string' == typeof globs ? [globs] : globs
+  const globsArray: string[] = 'string' == typeof globs ? [globs] : globs;
   const fontStream = new SVGIconsDirStream(
-    ([] as string[]).concat.apply([], globsArray.map(g => glob.sync(g))),
+    globsArray
+      .map((g) => glob.sync(g))
+      .reduce((acc, item) => acc.concat(item), []),
     options,
-  ).pipe(new SVGIcon2SVGFontStream(options))
+  )
+    .on('error', (err: Error) => {
+      resultStream.emit('error', new PluginError(PLUGIN_NAME, err));
+    })
+    .pipe(
+      new SVGIcons2SVGFontStream(options).on('error', (err: Error) => {
+        resultStream.emit('error', new PluginError(PLUGIN_NAME, err));
+      }),
+    );
   const fontVinyl = new Vinyl({
     contents: fontStream,
-  })
-  resultStream.push(fontVinyl)
-  resultStream.push(null)
-  return resultStream
-}
-export = svgicons2svgfont
+    path: `${options.fileName}.svg`,
+  });
+
+  resultStream.push(fontVinyl);
+  resultStream.push(null);
+  return resultStream;
+};
---
+++ src/tests/tests.test.ts
@@ -0,0 +1,406 @@
+import { describe, beforeEach, afterEach, test, expect } from '@jest/globals';
+import { Readable } from 'node:stream';
+import {} from 'node:fs';
+import { join } from 'node:path';
+import { readFile } from 'node:fs/promises';
+import type Vinyl from 'vinyl';
+
+import gulp from 'gulp';
+import { rimraf } from 'rimraf';
+import { mkdirp } from 'mkdirp';
+
+import assert from 'assert';
+import StreamTest from 'streamtest';
+
+import { svgicons2svgfont } from '../index.js';
+import { getMetadataService } from 'svgicons2svgfont';
+
+describe('gulp-svgicons2svgfont', () => {
+  beforeEach(async () => {
+    await mkdirp(join('src', 'tests', 'results'));
+  });
+
+  afterEach(async () => {
+    await rimraf(join('src', 'tests', 'results'));
+  });
+
+  describe('must emit an error', () => {
+    test('when a glyph is bad', async () => {
+      const [stream, result] = StreamTest.toObjects<Vinyl>();
+
+      try {
+        await Promise.all([
+          new Promise<void>((resolve, reject) => {
+            svgicons2svgfont(
+              join('src', 'tests', 'fixtures', 'invalidicon.svg'),
+              {
+                fontName: 'unprefixedicons',
+              },
+            )
+              .on('error', (err) => {
+                reject(err);
+              })
+              .pipe(stream);
+          }),
+          result,
+        ]);
+
+        throw new Error('E_UNEXPECTED');
+      } catch (err) {
+        expect((err as Error).message).toEqual(
+          'Non-whitespace before first tag.\nLine: 0\nColumn: 1\nChar: F',
+        );
+      }
+    });
+  });
+
+  describe('in stream mode', () => {
+    test('should work with cleanicons', async () => {
+      const [stream, result] = StreamTest.toObjects<Vinyl>();
+      const [contentStream1, contentResult1] = StreamTest.toChunks();
+
+      svgicons2svgfont(
+        join('src', 'tests', 'fixtures', 'cleanicons', '*.svg'),
+        {
+          fontName: 'cleanicons',
+          startUnicode: 0xe001,
+        },
+      ).pipe(stream);
+
+      const files = await result;
+
+      expect(files.length).toEqual(1);
+      expect(files[0].isStream()).toEqual(true);
+
+      (files[0].contents as Readable).pipe(contentStream1);
+
+      expect(Buffer.concat(await contentResult1).toString()).toEqual(
+        await readFile(
+          join('src', 'tests', 'expected', 'test-cleanicons-font.svg'),
+          'utf-8',
+        ),
+      );
+    });
+
+    test('should work with the metadataProvider option', async () => {
+      const [stream, result] = StreamTest.toObjects<Vinyl>();
+      const [contentStream1, contentResult1] = StreamTest.toChunks();
+
+      svgicons2svgfont(
+        join('src', 'tests', 'fixtures', 'cleanicons', '*.svg'),
+        {
+          fontName: 'cleanicons',
+          metadataProvider: getMetadataService({
+            startUnicode: 0xe001,
+          }),
+        },
+      ).pipe(stream);
+
+      const files = await result;
+
+      expect(files.length).toEqual(1);
+      expect(files[0].isStream()).toEqual(true);
+
+      (files[0].contents as Readable).pipe(contentStream1);
+
+      expect(Buffer.concat(await contentResult1).toString()).toEqual(
+        await readFile(
+          join('src', 'tests', 'expected', 'test-cleanicons-font.svg'),
+          'utf-8',
+        ),
+      );
+    });
+
+    test('should work with prefixedicons', async () => {
+      const [stream, result] = StreamTest.toObjects<Vinyl>();
+      const [contentStream1, contentResult1] = StreamTest.toChunks();
+
+      svgicons2svgfont(
+        join('src', 'tests', 'fixtures', 'prefixedicons', '*.svg'),
+        {
+          fontName: 'prefixedicons',
+          startUnicode: 0xe001,
+        },
+      ).pipe(stream);
+
+      const files = await result;
+
+      expect(files.length).toEqual(1);
+      expect(files[0].isStream()).toEqual(true);
+
+      (files[0].contents as Readable).pipe(contentStream1);
+
+      expect(Buffer.concat(await contentResult1).toString()).toEqual(
+        await readFile(
+          join('src', 'tests', 'expected', 'test-prefixedicons-font.svg'),
+          'utf-8',
+        ),
+      );
+    });
+
+    test('should work with originalicons', async () => {
+      const [stream, result] = StreamTest.toObjects<Vinyl>();
+      const [contentStream1, contentResult1] = StreamTest.toChunks();
+
+      svgicons2svgfont(
+        join('src', 'tests', 'fixtures', 'originalicons', '*.svg'),
+        {
+          fontName: 'originalicons',
+        },
+      ).pipe(stream);
+
+      const files = await result;
+
+      expect(files.length).toEqual(1);
+      expect(files[0].isStream()).toEqual(true);
+
+      (files[0].contents as Readable).pipe(contentStream1);
+
+      expect(Buffer.concat(await contentResult1).toString()).toEqual(
+        await readFile(
+          join('src', 'tests', 'expected', 'test-originalicons-font.svg'),
+          'utf-8',
+        ),
+      );
+    });
+
+    describe('more', () => {
+      beforeEach(async () => {
+        await new Promise((resolve, reject) =>
+          gulp
+            .src(join('src', 'tests', 'fixtures', 'unprefixedicons', '*.svg'))
+            .pipe(gulp.dest(join('src', 'tests', 'results', 'unprefixedicons')))
+            .on('error', reject)
+            .on('end', resolve),
+        );
+      });
+
+      test('should work with unprefixed icons and the prependUnicode option', async () => {
+        const [stream, result] = StreamTest.toObjects<Vinyl>();
+        const [contentStream1, contentResult1] = StreamTest.toChunks();
+
+        svgicons2svgfont(
+          join('src', 'tests', 'results', 'unprefixedicons', '*.svg'),
+          {
+            fontName: 'unprefixedicons',
+            prependUnicode: true,
+          },
+        ).pipe(stream);
+
+        const files = await result;
+
+        expect(files.length).toEqual(1);
+        expect(files[0].isStream()).toEqual(true);
+
+        (files[0].contents as Readable).pipe(contentStream1);
+
+        expect(Buffer.concat(await contentResult1).toString()).toEqual(
+          await readFile(
+            join('src', 'tests', 'expected', 'test-unprefixedicons-font.svg'),
+            'utf-8',
+          ),
+        );
+
+        const expectedRenames = {
+          'arrow-down.svg': 'uEA01-arrow-down.svg',
+          'arrow-left.svg': 'uEA02-arrow-left.svg',
+          'arrow-right.svg': 'uEA03-arrow-right.svg',
+          'arrow-up.svg': 'uEA04-arrow-up.svg',
+        };
+
+        for (const oldFile of Object.keys(expectedRenames)) {
+          const newFile = expectedRenames[oldFile];
+          const newFilePath = join(
+            'src',
+            'tests',
+            'results',
+            'unprefixedicons',
+            newFile,
+          );
+
+          expect(await readFile(newFilePath)).toBeTruthy();
+          expect(
+            await readFile(
+              join('src', 'tests', 'results', 'unprefixedicons', newFile),
+              'utf8',
+            ),
+          ).toEqual(
+            await readFile(
+              join('src', 'tests', 'fixtures', 'unprefixedicons', oldFile),
+              'utf8',
+            ),
+          );
+        }
+      });
+    });
+
+    describe('more2', () => {
+      beforeEach(async () => {
+        await new Promise((resolve, reject) =>
+          gulp
+            .src(join('src', 'tests', 'fixtures', 'unicons', '*.svg'))
+            .pipe(gulp.dest(join('src', 'tests', 'results', 'unicons')))
+            .on('error', reject)
+            .on('end', resolve),
+        );
+      });
+
+      test('should work with mixed icons and the prependUnicode option', async () => {
+        const [stream, result] = StreamTest.toObjects<Vinyl>();
+        const [contentStream1, contentResult1] = StreamTest.toChunks();
+
+        svgicons2svgfont(join('src', 'tests', 'results', 'unicons', '*.svg'), {
+          fontName: 'unicons',
+          prependUnicode: true,
+        }).pipe(stream);
+
+        const files = await result;
+
+        expect(files.length).toEqual(1);
+        expect(files[0].isStream()).toEqual(true);
+
+        (files[0].contents as Readable).pipe(contentStream1);
+
+        expect(Buffer.concat(await contentResult1).toString()).toEqual(
+          await readFile(
+            join('src', 'tests', 'expected', 'test-unicons-font.svg'),
+            'utf-8',
+          ),
+        );
+
+        const expectedRenames = {
+          'uEA01-twitter.svg': 'uEA01-twitter.svg',
+          'facebook.svg': 'uEA02-facebook.svg',
+        };
+
+        for (const oldFile of Object.keys(expectedRenames)) {
+          const newFile = expectedRenames[oldFile];
+          const newFilePath = join(
+            'src',
+            'tests',
+            'results',
+            'unicons',
+            newFile,
+          );
+
+          expect(await readFile(newFilePath)).toBeTruthy();
+          expect(
+            await readFile(
+              join('src', 'tests', 'results', 'unicons', newFile),
+              'utf8',
+            ),
+          ).toEqual(
+            await readFile(
+              join('src', 'tests', 'fixtures', 'unicons', oldFile),
+              'utf8',
+            ),
+          );
+        }
+      });
+    });
+
+    test('should emit an event with the codepoint mapping', async () => {
+      const [stream, result] = StreamTest.toObjects<Vinyl>();
+      const [contentStream1, contentResult1] = StreamTest.toChunks();
+
+      let codepoints;
+
+      svgicons2svgfont(
+        join('src', 'tests', 'fixtures', 'cleanicons', '*.svg'),
+        {
+          fontName: 'cleanicons',
+          startUnicode: 0xe001,
+        },
+      )
+        .on('glyphs', (c) => {
+          codepoints = c;
+        })
+        .pipe(stream);
+
+      const files = await result;
+
+      expect(files.length).toEqual(1);
+      expect(files[0].isStream()).toEqual(true);
+
+      (files[0].contents as Readable).pipe(contentStream1);
+
+      expect(Buffer.concat(await contentResult1).toString()).toEqual(
+        await readFile(
+          join('src', 'tests', 'expected', 'test-cleanicons-font.svg'),
+          'utf-8',
+        ),
+      );
+
+      expect(codepoints).toEqual(
+        JSON.parse(
+          (
+            await readFile(
+              join('src', 'tests', 'expected', 'test-codepoints.json'),
+            )
+          ).toString(),
+        ),
+      );
+    });
+
+    test('should support filename change', async () => {
+      const [stream, result] = StreamTest.toObjects<Vinyl>();
+      const [contentStream1, contentResult1] = StreamTest.toChunks();
+
+      svgicons2svgfont(
+        join('src', 'tests', 'fixtures', 'cleanicons', '*.svg'),
+        {
+          fontName: 'cleanicons',
+          fileName: 'newName',
+          startUnicode: 0xe001,
+        },
+      ).pipe(stream);
+
+      const files = await result;
+
+      expect(files.length).toEqual(1);
+      expect(files[0].isStream()).toEqual(true);
+      expect(files[0].path).toContain('newName.svg');
+
+      (files[0].contents as Readable).pipe(contentStream1);
+
+      expect(Buffer.concat(await contentResult1).toString()).toEqual(
+        await readFile(
+          join('src', 'tests', 'expected', 'test-cleanicons-font.svg'),
+          'utf-8',
+        ),
+      );
+    });
+  });
+
+  describe('must throw error', () => {
+    test('when no fontname is given', () => {
+      assert.throws(() => {
+        svgicons2svgfont(
+          ...([] as unknown as [
+            string,
+            {
+              fontName: string;
+            },
+          ]),
+        );
+      });
+    });
+
+    test('when using old options', () => {
+      assert.throws(() => {
+        svgicons2svgfont(
+          ...([
+            {
+              appendUnicode: true,
+            },
+          ] as unknown as [
+            string,
+            {
+              fontName: string;
+            },
+          ]),
+        );
+      });
+    });
+  });
+});
